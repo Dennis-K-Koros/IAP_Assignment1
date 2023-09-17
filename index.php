8b7f563f1e49f249d8792d01af881336c2968b2a
Savayi <savayichelsea90@gmail.com>
2023-09-17 21:24:42 +0300

    Modified

--- index.php
+++ index.php
@@ -11,26 +11,21 @@
             <h1>Contact Us</h1>
                 <form action="mail.php" method="post" autocomplete="on">
                     <div class = "user-box">
-                    <input type="text" id="name" name="name" required="">
-                    <label for="name"> Name</label><br><br>
+                        <input type="text" id="name" name="name" required="">
+                        <label for="name"> Name</label><br><br>
                     </div>
                     <div class = "user-box">
-                    <input type="email" id="email" name="email" required="">
-                    <label for="email"> Email</label><br><br>
+                        <input type="email" id="email" name="email" required="">
+                        <label for="email"> Email</label><br><br>
                     </div>
                     <div class = "user-box">
-                    <textarea id="message" name="message" rows="2" required=""></textarea>
-                    <label for="message"> Message</label><br><br>
+                        <textarea id="message" name="message" rows="1" required=""></textarea>
+                        <label for="message"> Message</label><br><br>
                     </div>
-                    <div class ="btn">
-                        <a href ='#'>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            Submit
-                        </a>
-                    </div>  
+                    <div class="btn">
+                        <button type="submit" name="submit">Submit</button>
+                    </div>
+  
                 </form>
         </div>
     </body>
