9f5d6477a93597641cb64a02577402e8aa552ee3
Savayi <savayichelsea90@gmail.com>
2023-09-21 14:09:45 +0300

    changes

--- index.php
+++ index.php
@@ -1,32 +1,63 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Form</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Welcome to Our Website</title>
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
 </head>
-    <body>
-        <div class = "login-box">
-            <h1>Contact Us</h1>
-                <form action="mail.php" method="post" autocomplete="on">
-                    <div class = "user-box">
-                        <input type="text" id="name" name="name" required="">
-                        <label for="name"> Name</label><br><br>
-                    </div>
-                    <div class = "user-box">
-                        <input type="email" id="email" name="email" required="">
-                        <label for="email"> Email</label><br><br>
-                    </div>
-                    <div class = "user-box">
-                        <textarea id="message" name="message" rows="1" required=""></textarea>
-                        <label for="message"> Message</label><br><br>
-                    </div>
-                    <div class="btn">
-                        <button type="submit" name="submit">Submit</button>
-                    </div>
-  
-                </form>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">Your Logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contactus.php">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <section class="hero bg-primary text-light py-5">
+            <div class="container text-center">
+                <h1 class="display-4">Welcome to Our Website</h1>
+                <p class="lead">Discover amazing content and more.</p>
+                <a class="btn btn-light btn-lg" href="contactus.php">Contact Us</a>
+            </div>
+        </section>
+        
+        <section class="content py-5">
+            <div class="container">
+                
+            </div>
+        </section>
+    </main>
+
+    <footer class="bg-light py-4">
+        <div class="container text-center">
+           
+            <p>&copy; 2023 Your Website</p>
         </div>
-    </body>
-</html>
+    </footer>
+
+    <
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html> -->
