47904006ec04e6a47da3141bc499893bb4a9c107
unknown <ian.karanja@strathmore.edu>
2023-09-23 21:25:53 +0300

    Modified db.sql and dbconn

--- sql/db.sql
+++ sql/db.sql
@@ -10,12 +10,12 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
     `userId` bigint(10) NOT NULL AUTO_INCREMENT,
     `fullname` VARCHAR(50) NOT NULL DEFAULT '',
-    `email_address` varchar(50) NOT NULL DEFAULT '',
+    `email` varchar(50) NOT NULL DEFAULT '',
     `password` varchar(60) NOT NULL DEFAULT '',
     `pass_token` varchar(32) NOT NULL DEFAULT '',
     `pass_token_expire` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`userId`),
-    UNIQUE KEY `email_address` (`email_address`)
+    UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
